96f7403361304d87ea8468ba48cef55e1afdd03d
Glaze96 <rasmusramen@gmail.com>
2023-11-23 12:59:43 +0100

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+	return (
+		<main className="">
+			<h1 className="text-center text-lg">Site under construction</h1>
+			<h2 className="text-center text-sm">(It will be made by coffee)</h2>
+		</main>
+	);
+}
